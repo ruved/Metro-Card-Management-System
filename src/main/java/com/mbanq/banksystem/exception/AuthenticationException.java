d2a9f9af4536cc14a967c318cfeaeed7887d990c
Ruved Aalam <ruvedalam7776@gmail.com>
2024-04-15 20:05:06 +0530

    Delete src directory

--- src/main/java/com/mbanq/banksystem/exception/AuthenticationException.java
+++
@@ -1,7 +0,0 @@
-package com.mbanq.banksystem.exception;
-
-public class AuthenticationException extends RuntimeException {
-    public AuthenticationException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
